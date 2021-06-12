8038c028ace44dd4eaa4f7f4a7e74e4d8e96726b
nazarov-in <47144687+nazarov-in@users.noreply.github.com>
2021-06-12 16:02:53 +0300

    update 0.1

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const slides = document.querySelectorAll('.slide')
+const slidesPlagin = (slideActive = 2) => {
+    const slides = document.querySelectorAll('.slide')
 
-slides.forEach(slide => {
-    slide.addEventListener('click', () => {
-        clearActiveClasses()
-        slide.classList.add('active')
+    slides.forEach(slide => {
+        slide.addEventListener('click', () => {
+            clearActiveClasses()
+            slide.classList.add('active')
+        })
     })
-})
 
-const clearActiveClasses = () => {
-    slides.forEach((slide) => {
-        slide.classList.remove('active')
-    })
+    const clearActiveClasses = () => {
+        slides.forEach((slide) => {
+            slide.classList.remove('active')
+        })
+    }
 }
+
+slidesPlagin(3)
